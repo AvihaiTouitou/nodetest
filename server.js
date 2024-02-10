4b37cc90c1db079edbaceb7dc37c9915fd0bac32
AvihaiTouitou <avihai.t104@gmail.com>
2024-02-10 16:40:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const cors=require('cors');
+
+app=express();
+
+app.use(cors());
+
+app.listen(8000,()=>{
+    console.log(`server listen port 8000`);
+})
+
+app.get('/test',(req,res)=>res.send("ok"));
+
+
+
